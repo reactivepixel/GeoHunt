98926d59c290b92cf9fbab53030f3bf1574f41a0
Christopher Chapman <cchapman@fullsail.com>
2013-12-15 15:30:30 -0500

    v1 ready to go. Updating FB normally

--- inc/js/main.js
+++ inc/js/main.js
@@ -4,37 +4,13 @@ angular.module('githubAuth', ['ngRoute','firebase'])
 	// Routes
 	r
 	.when('/',{
-		templateUrl : 'views/home.tpl',
+		templateUrl : 'views/dashboard.tpl',
+		controller 	: 'Dashboard',
 	})
 
 	.when('/dashboard',{
-		templateUrl : 'views/status.tpl',
+		templateUrl : 'views/dashboard.tpl',
 		controller 	: 'Dashboard',
-	})
-	.when('/loc',{
-		templateUrl : 'views/loc.tpl',
-	})
-	.when('/easy',{
-		templateUrl : 'views/easy.tpl',
-	})
-	.when('/add', {
-		templateUrl : 'views/add.tpl',
-		controller	: 'AddWaypoint'
-	})
-	.when('/stepTwo', {
-		templateUrl : 'views/home.tpl',
-		controller	: 'FireLogic'
-	})
-	.when('/GHID/:id', {
-		templateUrl : 'views/status.tpl',
-		controller	: 'Status'
-	})
-	.when('/added', {
-		templateUrl : 'views/added.tpl',
-	})
-	.when('/error/:msg', {
-		templateUrl : 'views/error.tpl',
-		controller	: 'ErrorLogic'
 	})
 
 }])
@@ -49,6 +25,7 @@ angular.module('githubAuth', ['ngRoute','firebase'])
                         function (position) {
                             $rootScope.$apply(function () {
                                 console.log('Updating Loc', position);
+                                $rootScope.Data = position;
                                 d.resolve({
                                     data: position
                                 });
@@ -72,157 +49,28 @@ angular.module('githubAuth', ['ngRoute','firebase'])
 
 
 // Core Controller
-.controller('AuthLogic', ['$scope', 'locService', '$firebase', '$firebaseAuth', '$location',function(s,locService,$firebase,$firebaseAuth,$location){
+.controller('AuthLogic', ['$scope', 'locService', '$firebase', '$firebaseAuth', '$location', '$rootScope', function(s,locService,$firebase,$firebaseAuth,$location,$rootScope){
 	s.title = 'Time to show a detail Page Derp derp';
 	s.user;
 
-	var url = 'https://chapman.firebaseio.com';
-	var sync = $firebase(new Firebase(url)).$bind(s, 'Data');
+	var url = 'https://chapman.firebaseio.com/currentLoc';
+	var sync = $firebase(new Firebase(url)).$bind($rootScope, 'Data');
 	
-	var authRef = new Firebase(url);
-    s.auth = $firebaseAuth(authRef);
-
 
 
     s.loc = locService();
 
-	//Firebase Simple Login Auth Call
-	s.authenticateMe = function(){
-		s.auth.$login('github', {
-			rememberMe: true,
-	  		scope: 'user,repo'
-		}).then(function(user){
-			s.regUser(user);
-		});
-	}
 
 
-	s.regUser = function(user){
 
-		user.progress = 0;
-
-		if(s.Data.activeUsers == undefined){
-			s.Data.activeUsers = [];
-		}
-
-		//if user is new
-		if(s.Data.activeUsers[user.id] == undefined){
-			s.Data.activeUsers[user.id] = user;
-		}
-		$location.path('/add');
-	}
 }])
 
 // Dashboard Controller
-.controller('Dashboard', ['$scope', '$routeParams', function(s,params){
+.controller('Dashboard', ['$scope', '$routeParams', '$rootScope', function(s,params,$rootScope){
 
 	// Don't trust current Firebase connection, create new, and apply error to log
 	//var fbError = new Firebase('https://chapman.firebaseio.com/errors');
 	
-	s.waypoints = s.Data.locations[s.user.progress];
+	
 
 }])
-
-// Error Controller
-.controller('ErrorLogic', ['$scope', '$routeParams', function(s,params){
-
-	// Don't trust current Firebase connection, create new, and apply error to log
-	//var fbError = new Firebase('https://chapman.firebaseio.com/errors');
-	s.Data.errors.push({params:params});
-	s.errorMsg = 'An error has occurred. [code: ' + params.msg + ']';
-	
-}])
-
-// Error Controller
-.controller('AddWaypoint', ['$scope', '$routeParams', function(s,params){
-
-	// Don't trust current Firebase connection, create new, and apply error to log
-	s.pushWaypoint = function(){
-		if(s.Data.locations == undefined){
-			s.Data.locations = [];
-		}
-		s.Data.locations.push({title: s.TitleInput, clue: s.ClueInput, instruction: s.InstructionInput, lat: s.LatInput,lon: s.LonInput, code: s.CodeInput});
-		console.log('added waypoint');
-	}
-
-	//s.errorMsg = 'An error has occurred. [code: ' + params.msg + ']';
-	
-}])
-
-
-// Status Controller
-.controller('Status', ['$scope', '$routeParams', function(s,params){
-	fb.on('value', function(data){
-		var activeUsers = data.val();
-		for(user in activeUsers){
-			if(activeUsers[user].GHID == params.id){
-				s.user = activeUsers[user];
-				s.user.fbid = user;
-				break;
-			}
-		}
-		console.log('User Info Modified', s.user);
-	});
-
-	s.submitCode = function(subCode){
-		//s.FoundCodeInput
-		for(waypoint in s.fbWaypoints){
-
-			if(subCode == s.fbWaypoints[waypoint].code){
-				var tarURL = 'https://chapman.firebaseio.com/activeUsers/' + s.user.fbid + "/waypoints";
-				console.log('Selected to add', s.fbWaypoints[waypoint]);
-				var fbUserWaypoints = new Firebase(tarURL);
-				fbUserWaypoints.push(s.fbWaypoints[waypoint]);
-				s.unlocked = true;
-				setTimeout(function(){
-					s.unlocked = false;
-				},2000);
-				return true;
-				break;
-			}
-		}
-		
-		console.log('match not found');
-
-	}
-}])
-
-
-
-// Second Step Controller
-.controller('FireLogic', ['$scope', function(s){
-	
-	// Check local Storage, if nothing is there, you shouldn't be on this page... Dump out to Error Screen
-	if(!localUser){
-		window.location = "#/error/Skipped A Step"
-	} else {
-		//s.displayName = localUser.displayName;
-		//window.location = "#/GHID/" + s.user.GHID;
-
-		s.activeUserLookup();
-		// On Email Verification Submit
-		
-	}
-	
-	//
-}]);
-
-var localUser;
-
-//Generic FB Connection. Could be integrated into Core Controller.
-var fb = new Firebase('https://chapman.firebaseio.com/activeUsers');
-var fbWaypoints = new Firebase('https://chapman.firebaseio.com/waypoints');
-var auth = new FirebaseSimpleLogin(fb, function(error, user) {
-  if (error) {
-    // an error occurred while attempting login
-    window.location = "#/error/Database failure - " + error;
-  } else if (user) {
-    // user authenticated with Firebase
-    localUser = user;
-    window.location = '#/stepTwo';
-//	fb.push({email:'force', githubUserInfo: user});
-
-  } else {
-    // user is logged out
-  }
-});
--- views/dashboard.tpl
+++ views/dashboard.tpl
@@ -1,1 +1,4 @@
-Loaded
+<h3>{{Data.timestamp | date:'medium'}}</h3>
+<ul>
+<li ng-repeat="item in Data.coords">{{item}}</li>
+</ul>
